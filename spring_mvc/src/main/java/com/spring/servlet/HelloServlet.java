673a985ed95fec1d1eda6369a6541d12f4a948d7
pps4513@naver.com <pps4513@naver.com>
2024-01-04 17:57:29 +0900

    1월 4일 스프링 MVC

---
+++ spring_mvc/src/main/java/com/spring/servlet/HelloServlet.java
@@ -0,0 +1,28 @@
+package com.spring.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@WebServlet("/hello.html")
+public class HelloServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String url = "/WEB-INF/views/hello.jsp";
+		
+//		String who = request.getParameter("who");
+//		String message = request.getParameter("message");
+		
+		request.getRequestDispatcher(url).forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
---
+++ spring_mvc/src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>hello.jsp</h1>
+	<h1>TO : ${param.who }</h1>
+	<h1>message : ${param.message }</h1>
+</body>
+</html>
