dcae57eaa21a7cf491b812c2c86d2d5e16f44245
pps4513@naver.com <pps4513@naver.com>
2024-01-08 05:48:24 +0900

    1월 8일

--- spring_mybatis/src/test/java/com/spring/dao/TestMemberDAOImpl.java
+++ spring_mybatis/src/test/java/com/spring/dao/TestMemberDAOImpl.java
@@ -13,8 +13,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.spring.dto.MemberVO;
 
-
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:com/spring/context/root-context.xml")
 @Transactional
